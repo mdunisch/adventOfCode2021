9b8c6b091f329d91e9223e4b2735ca0d1ac0d829
mdunisch <martin.dunisch@gmail.com>
2021-12-12 09:52:12 +0100

    init commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  singleQuote: true,
+  useTabs: false,
+  printWidth: 120,
+  trailingComma: 'none',
+  arrowParens: 'avoid'
+};
---
+++ 01/01a.ts
@@ -0,0 +1,7 @@
+const file = 'Dasd';
+
+function add(a: number, b: number): number {
+  return 1;
+}
+
+console.log(add(1, 2));

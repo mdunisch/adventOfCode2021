f8d782a35d33fd218048bd1aa02d5909c647ff5d
mdunisch <martin.dunisch@gmail.com>
2021-12-12 10:18:54 +0100

    Day 1

--- 01/01a.ts
+++ 01/01a.ts
@@ -1,7 +1,14 @@
-const file = 'Dasd';
+import { readFileSync } from 'fs';
 
-function add(a: number, b: number): number {
-  return 1;
-}
+const file = readFileSync('./01/input01a.txt', 'utf8')
+  .split('\n')
+  .map((i: string) => parseInt(i));
 
-console.log(add(1, 2));
+const solution = file.reduce((acc, currentDepth, index) => {
+  if (currentDepth > file[index - 1]) {
+    acc++;
+  }
+  return acc;
+}, 0);
+
+console.log(solution);
---
+++ 01/01b.ts
@@ -0,0 +1,26 @@
+import { readFileSync } from 'fs';
+
+const file = readFileSync('./01/input01b.txt', 'utf8')
+  .split('\n')
+  .map((i: string) => parseInt(i));
+
+// 1. build array of measurement windows
+const windows = [];
+for (let i = 0; i < file.length; i++) {
+  if (i + 3 > file.length) {
+    break;
+  }
+  // calculate sum of window
+  const window = file.slice(i, i + 3).reduce((a, b) => a + b);
+  windows.push(window);
+}
+
+// 2. calculte solution
+const solution = windows.reduce((acc, currentDepth, index) => {
+  if (currentDepth > windows[index - 1]) {
+    acc++;
+  }
+  return acc;
+}, 0);
+
+console.log(solution);
